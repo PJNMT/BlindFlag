546087fe7e91e9a85404371116079c7d10734050
Tao Blancheton <tao.blancheton@epita.fr>
2019-01-23 15:54:45 +0100

    deb

---
+++ Game/Game/Pirate.cs
@@ -0,0 +1,10 @@
+namespace Game
+{
+    public class Pirate : Ship
+    {
+        public Pirate(int Gold, int Level, int HP, int Attack_pts, int Speed, int Crew)
+        {
+            
+        }
+    }
+}
