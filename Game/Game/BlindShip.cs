33e5f22e6c1c182416a7bebbc2c2c523705a2a05
marius.hurbin <marius.hurbin@epita.fr>
2019-01-23 16:56:04 +0100

    je suis la !

--- Game/Game/BlindShip.cs
+++ Game/Game/BlindShip.cs
@@ -3,5 +3,17 @@ namespace Game
     public class BlindShip : Ship
     {
         private int test;
+
+        public BlindShip()
+        {
+            this.Gold = ;
+            this.Level = Level;
+            this.HP = HP;
+            this.Attack_pts = Attack_pts;
+            this.Speed = Speed;
+            Nb_Crew = Crew;
+        }
+        
+        
     }
 }
