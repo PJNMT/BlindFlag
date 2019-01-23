c1f39c5c1979c0be6bc90812b5bdfb0436498b76
marius.hurbin <marius.hurbin@epita.fr>
2019-01-23 16:51:46 +0100

    je suis la !

--- Game/Game/Place.cs
+++ Game/Game/Place.cs
@@ -2,10 +2,8 @@ namespace Game
 {
     public abstract class Place
     {
-        private int Next_Place; (* distance avant *)
-
-
-
+        private int Next_Place; 
+        /* nb d'evenements avant la prochaine place */
 
         public int GetNextPlace()
         {
