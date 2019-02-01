ffc92c6454f5fa8a7bb8a12e18fdfdc589a7a8a0
Tao Blancheton <tao.blancheton@epita.fr>
2019-02-02 00:40:16 +0100

    Reconnaissance Vocal

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Speech.Recognition;
 
 namespace Game
 {
-  internal class Program
-  {
-    public static void Main()
+    class Program
     {
-      Console.Write("BlindFlag");
+        static void Main(string[] args)
+        {             
+            using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine(new System.Globalization.CultureInfo("fr-FR")))
+            {  
+                recognizer.LoadGrammar(new DictationGrammar());
+ 
+                recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+  
+                recognizer.SetInputToDefaultAudioDevice();
+  
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+  
+                while (true)
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+  
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine("Recognized text: " + e.Result.Text);
+        }
     }
-  }
 }
