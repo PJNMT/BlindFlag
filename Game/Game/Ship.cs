84e887d245cb8d1823af47b488b2663411332726
Tao Blancheton <tao.blancheton@epita.fr>
2019-02-19 01:49:16 +0100

    ShipShop

--- Game/Game/Ship.cs
+++ Game/Game/Ship.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Net.NetworkInformation;
-
 namespace Game
 {
     public abstract class Ship
     {
         protected bool Hostility { get; set; }
 
-        protected string Name{ get; set; }
-        protected int Gold{ get; set; }
+        protected string Name{ get; }
+        public int Gold{ get; set; }
         
         protected int Level{ get; set; }
         protected int XP{ get; set; }
         
-        protected int HP{ get; set; }
+        public int HP{ get; set; }
+        public int Max_HP{ get; set; }
         protected int Attack_pts{ get; set; }
         protected int Speed { get; set; }
 
@@ -22,9 +20,5 @@ namespace Game
 
         protected int PosX{ get; set; }
         protected int PosY{ get; set; }
-
-        
-
-
-       }
+    }
 }
--- Game/Game/ShipShop.cs
+++ Game/Game/ShipShop.cs
@@ -1,10 +1,60 @@
+using System.Speech.Recognition;
+
 namespace Game
 {
     public class ShipShop
     {
-        public ShipShop()
+        public ShipShop() { }
+
+        private void repair(int cost, Captain captain)
         {
+            Ship ship = captain.batiment;
+            ship.Gold -= cost;
+            ship.HP = ship.Max_HP;
+        }
+
+        private void up_captain(Captain captain, int status = 0)
+        {
+            StT recognition = new StT(new Choices("fusil", "épée", "quiter"));
+            string choice;
+
             // TODO
+            // si status == 0: marchand dit bonjour ...
+            // si status != 0: marchand dit merci d'avoir achter chez nous ...
+            // marchand dit notre argent total
+            // marchand propose : fusil, épée, quiter
+            choice = recognition.GetSpeech(5);
+
+            while (choice != "fusil" || choice != "épée" || choice != "quiter")
+            {
+                // marchand n'a pas bien compris et repropose : fusil, épée, quiter
+                choice = recognition.GetSpeech(5);
+            }
+
+            if (choice != "quiter")
+            {
+                switch (choice)
+                {
+                    case "fusil":
+                        // TODO
+                        // calcul du prix de l'amelioration
+                        // ssi assez argent => marchand dit le prix et propose une validation
+                        // sinon => marchand dit qu'on a pas assez d'argent
+                        break;
+
+                    case "épée":
+                        // TODO
+                        // calcul du prix de l'amelioration
+                        // ssi assez argent => marchand dit le prix et propose une validation
+                        // sinon => marchand dit qu'on a pas assez d'argent
+                        break;
+                }
+
+                up_captain(captain, 1);
+            }
+
+            // TODO
+            // marchand dit aurevoir ...
         }
     }
 }
