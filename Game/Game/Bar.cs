b6d85ca18384f829a873335b37c87c85f072beda
Néphélie <lambrinephelie@hotmail.fr>
2019-03-10 18:28:55 +0100

    début classe bar

--- Game/Game/Bar.cs
+++ Game/Game/Bar.cs
@@ -14,9 +14,23 @@ namespace Game
             I_am_drinking_every_day
         }
 
+        public static void launch_bar(Captain captain, int statut 0)
+        {
+            StT recognition = new StT(new Choices("quitter", "simon", "payer une tournée", "recruter"));
+            string choice;
+
+            choice = recognition.GetSpeech(5);
+            
+            //TODO
+            //Coco donne le nombre d'équipage.
+            
+            
+
+        }
+
         public Bar()
         {
-            peoples = r.Next(20) + 10;
+            this.peoples = r.Next(20) + 10;
         }
     }
 }
