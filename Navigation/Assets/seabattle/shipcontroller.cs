3cd99c2093861e355540c2eaf9f67fd38da00981
Jeanne Morin <jeanne.morin@epita.fr>
2019-03-14 17:38:10 +0100

    ouqshvisquvbsyvukysvusvuysqd

--- Navigation/Assets/seabattle/shipcontroller.cs
+++ Navigation/Assets/seabattle/shipcontroller.cs
@@ -16,17 +16,17 @@ public class shipcontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-	    /*if (Input.GetKey(KeyCode.Space))
+	    if (Input.GetKey(KeyCode.Space))
 	    {
 		    m_speed = 1f;
-	    }*/ // ne fonctionne pas passe à travers les murs
+	    }
 	    Vector3 move = new Vector3();
 
 	    // Récupération des touches haut et bas
 	    if(Input.GetKey(KeyCode.UpArrow))
-		    move.x += m_speed;
-	    if(Input.GetKey(KeyCode.DownArrow))
 		    move.x -= m_speed;
+	    if(Input.GetKey(KeyCode.DownArrow))
+		    move.x += m_speed;
 
 	    // Récupération des touches gauche et droite
 	    if(Input.GetKey(KeyCode.LeftArrow))
