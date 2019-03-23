9f3d51dc4ae3b220143d73fff527943c3a18a80c
marius.hurbin <marius.hurbin@epita.fr>
2019-03-23 18:04:14 +0100

    le bateau se deplace tellement mieux !!

--- Navigation/Assets/seabattle/shipcontroller.cs
+++ Navigation/Assets/seabattle/shipcontroller.cs
@@ -1,25 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class shipcontroller : MonoBehaviour
 {
-	public float m_speed = 0.5f;
+	public float moveSpeed = 20f;
+	public float turnSpeed = 50f;
 	
 	
     // Start is called before the first frame update
-    void Start()
+    /*void Start()
     {
         transform.position = new Vector3(2f, 2f, 0f);
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
-	    if (Input.GetKey(KeyCode.Space))
-	    {
-		    m_speed = 1f;
-	    }
+
+	    if (Input.GetKey(KeyCode.UpArrow)) moveSpeed += 1f;
+	    if (Input.GetKey(KeyCode.DownArrow)) moveSpeed -= 1f;
+	    if(Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+	    if (Input.GetKey(KeyCode.Space)) moveSpeed += 1f;
+	    
+	    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+	    
+	    /*
 	    Vector3 move = new Vector3();
 
 	    // Récupération des touches haut et bas
@@ -34,20 +42,20 @@ public class shipcontroller : MonoBehaviour
 		    move.z -= m_speed;
 		    move.x += m_speed;
 	    }
-
 	    if (move.x > 0)
 	    {
 		    move.x = 0;
 	    }
-	    
-	    // Récupération des touches gauche et droite
-	    if(Input.GetKey(KeyCode.LeftArrow))
-		    move.z -= m_speed;
-	    if(Input.GetKey(KeyCode.RightArrow))
-		    move.z += m_speed;
+        // Récupération des touches gauche et droite
+	    if (Input.GetKey(KeyCode.LeftArrow)) rot += 3;
+		if(Input.GetKey(KeyCode.RightArrow)) rot -= 3;
 
+	    if (rot > 180) rot -= 180;
+	    if (rot < -180) rot += 180;
+	    
+	    
 	    // On applique le mouvement à l'objet
-	    transform.position += move;
+	    transform.position += move;*/
         
         		
     }
