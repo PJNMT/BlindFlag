fa18f50ed21ca09752f4997db6bbfb94f66fb5c1
Néphélie <lambrinephelie@hotmail.fr>
2019-03-26 22:27:51 +0100

    début ia

---
+++ Navigation/Assets/combat/EnnemyControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyControler : MonoBehaviour
+{
+    private float x_rand;
+    private float z_rand;
+    Random random = new Random();
+
+    private Vector3 position;
+
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position.Set(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rng = random;
+        while ((x_rand>5) || (z_rand>5) || (x_rand<-5) || (z_rand<-5) || (position != target.position) )
+        {
+            x_rand = 
+            position.Set(x_rand, 0, z_rand);
+        }
+    }
+}
