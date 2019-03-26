d53401efebbc8c6481bb872dd980a924ba4cbcb6
Néphélie <lambrinephelie@hotmail.fr>
2019-03-26 20:52:21 +0100

    rotation

--- Navigation/Assets/combat/deplacement.cs
+++ Navigation/Assets/combat/deplacement.cs
@@ -17,10 +17,12 @@ public class deplacement : MonoBehaviour
     private KeyCode intputarrière;
     private KeyCode intputdroit;
     private KeyCode intputgauche;
-    
-    
-    
-    
+    public float moveSpeed;
+    public float turnSpeed;
+
+
+
+
     /*public deplacement (int posx, int posy, int posz)
     {
       this.x = posx;
@@ -32,10 +34,12 @@ public class deplacement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        intputarrière = KeyCode.W;
+        intputarrière = KeyCode.S;
         intputavant = KeyCode.Z;
-        intputdroit = KeyCode.S;
+        intputdroit = KeyCode.D;
         intputgauche = KeyCode.Q;
+
+        turnSpeed = 500f;
         
         playercollider = gameObject.GetComponent<CapsuleCollider>();
     }
@@ -47,19 +51,19 @@ public class deplacement : MonoBehaviour
         
         if (Input.GetKeyDown(intputgauche))
         {
-            transform.Translate(1, 0, 1);
+            transform.Rotate(Vector3.up*turnSpeed*Time.deltaTime);
         }
         if (Input.GetKeyDown(intputdroit))
         {
-            transform.Translate(-1,0,-1);
+            transform.Rotate(-Vector3.up*turnSpeed*Time.deltaTime);
         }
         if (Input.GetKeyDown(intputavant))
         {
-            transform.Translate(1, 0, -1);
+            transform.Translate(1, 0, 0);
         }
         if (Input.GetKeyDown(intputarrière))
         {
-            transform.Translate(-1, 0, 1);
+            transform.Translate(-1, 0, 0);
         }
         
         if (Input.GetKeyDown(KeyCode.UpArrow)) //&& ausol())
