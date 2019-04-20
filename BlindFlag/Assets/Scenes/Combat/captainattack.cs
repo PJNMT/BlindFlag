604c0c73772163e2c206701271b8211f1f662094
Lyrianna <lambrinephelie@hotmail.fr>
2019-04-20 15:58:41 +0200

    debut script attack
    '-'

---
+++ BlindFlag/Assets/Scenes/Combat/captainattack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class captainattack : MonoBehaviour
+{
+    private int gun_atk;
+    private int saber_atk;
+    private int HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun_atk = BlindCaptain_Stat.GunDamage;
+        saber_atk = BlindCaptain_Stat.SwordDamage;
+        HP = BlindCaptain_Stat.HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
