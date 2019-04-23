f03a181a16dad32d07464e9245256d4efa33ba64
Lyrianna <lambrinephelie@hotmail.fr>
2019-04-23 16:01:13 +0200

    cbt

--- BlindFlag/Assets/Scenes/Combat/Tir.cs
+++ BlindFlag/Assets/Scenes/Combat/Tir.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Tir : MonoBehaviour
 {
 
-    public static GameObject Projectile;
+    public GameObject Projectile;
     public int Force = 50;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(captainattack.gunatk))
+        if (Input.GetKeyDown(captainattack.gunatk))
         {
             GameObject Bullet = Instantiate(Projectile, transform.position, Quaternion.identity); //create a new bullet
             Bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward) * Force; //apply a force on it
