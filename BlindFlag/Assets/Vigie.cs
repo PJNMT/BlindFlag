32b0ef29bfad4d844ff529d7e456853551044c99
marius.hurbin <marius.hurbin@epita.fr>
2019-04-25 15:09:23 +0200

    LAAAAAAAA vigie

--- BlindFlag/Assets/Vigie.cs
+++ BlindFlag/Assets/Vigie.cs
@@ -1,61 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class Vigie : MonoBehaviour
 {
-    private string trucAdire = "Chose vers ";
+    private Dictionary<GameObject, string> ObjetsVus;
+    public List<string> Tags;
+    
+    private void Start()
+    {
+        ObjetsVus = new Dictionary<GameObject, string>();
+        Tags = new List<string>(){"Ennemy","Ile","Visible"};
+        
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
-
+        if(!Tags.Contains(other.tag)) return;
         
-       /* if (other.GetComponent(typeof(WhatToSay)) )
-        {
-            trucAdire = other.name + " vers ";
-            
-            bool babord = (other.transform.position.x - this.transform.position.x < -75);
-            bool tribord = (other.transform.position.x - this.transform.position.x > 75);
-            bool devant = !(babord || tribord) && other.transform.position.z > transform.position.z;
-            bool derriere = !(babord || tribord) && other.transform.position.z <= transform.position.z;
-
-            if (transform.rotation.eulerAngles.y > 45 && transform.rotation.eulerAngles.y < 135) ;
+        ObjetsVus.Add(other.gameObject,other.tag);
         
         
         
-        
-            if(derriere) trucAdire += " le sud "/*derriere.ToString()*/;
-            //if(babord) trucAdire += " l'ouest "/*babord.ToString()*/;
-           // if(tribord) trucAdire += " l'est "/*tribord.ToString()*/;
-           // if(devant) trucAdire += " le nord "/*devant.ToString()*/;
-        
-           // Debug.Log(trucAdire);
-            //Synthesis.synthesis(trucAdire);
-            //trucAdire = other.name + " vers ";
-
-       // }
-        
-        
-
-        /*bool babord = (other.transform.position.x - this.transform.position.x < -75);
-        bool tribord = (other.transform.position.x - this.transform.position.x > 75);
-        bool devant = !(babord || tribord) && other.transform.position.z > transform.position.z;
-        bool derriere = !(babord || tribord) && other.transform.position.z <= transform.position.z;
-
-        if(transform.rotation.eulerAngles.y > 45 && transform.rotation.eulerAngles.y<135)
-        
-        
-        
-        
-     /*   if(derriere) trucAdire += " le sud "/*derriere.ToString()*/;
-      /*  if(babord) trucAdire += " l'ouest "/*babord.ToString()*/;
-       /* if(tribord) trucAdire += " l'est "/*tribord.ToString()*/;
-      /*  if(devant) trucAdire += " le nord "/*devant.ToString()*/;
-        /*
-        Debug.Log(trucAdire);
-        trucAdire = "chose a "; */
     }
-    
-    
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (ObjetsVus.ContainsKey(other.gameObject))
+        {
+            //J'ai perdu 'other' de vu
+            Synthesis.synthesis("J'ai perdu " + other.name + " de vue");
+            
+            
+            ObjetsVus.Remove(other.gameObject);
+        }
+            
+            
+    }
 }
 
