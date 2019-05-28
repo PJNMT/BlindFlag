54fd6e1729e589d2da96e73e041d66ff4a1c832b
Jeanne Morin <jeanne.morin@epita.fr>
2019-05-28 16:32:59 +0200

    coco fonctionne

--- BlindFlag/Assets/Coco.cs
+++ BlindFlag/Assets/Coco.cs
@@ -4,98 +4,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System.Threading;
+using static System.Threading.Thread;
 using static UnityEngine.Time;
 
 public class Coco : MonoBehaviour
 {
     public static bool TutoON;
+    public bool activated;
+    public bool quit;
     
-    void Cocotraitement(string words)
+    
+    void Answertraitement(string word)
     {
-        Debug.Log(words);
-        string[] decoupe = words.Split(' ');
-        foreach (string word in decoupe)
-        {
-            switch (word)
+        Debug.Log(word);
+           switch (word)
             {
                case "niveau":
+                   
                     Synthesis.synthesis("Blindcaptain niveau" + BlindCaptain_Stat.Lvl);
                     break;
 
                 case "bateau":
                 case "navire":
+                    
                     Synthesis.synthesis(" Tu as un bateau niveau " + BlindShip_Stat.Lvl +
-                                        " avec " + BlindShip_Stat.Crew + "homme d'équipage." +
+                                        " avec " + BlindShip_Stat.Crew + " homme d'équipage.  " +
                                         " Il peut contenir au maximum "
-                                        + BlindShip_Stat.Max_Crew +
+                                        + BlindShip_Stat.Max_Crew + "matelots.   " +
                                         " Ton bateau possède " + BlindShip_Stat.XP + " X P " +
                                         BlindShip_Stat.HP + " H P" + "et peut causer"
                                         + BlindShip_Stat.Damage + " point de dommage." + " Enfin, il a "
                                         + BlindShip_Stat.Shield + " point de capacité de défense");
-                    Thread.Sleep(10000);
+                    Sleep(15000);
                     break;
 
 
                 case "vie":
                 case "HP":
-                    Synthesis.synthesis("vie");
                     Synthesis.synthesis("Tu as " + BlindCaptain_Stat.HP + " point de vie." + "Ton bateau à " +
                                         BlindShip_Stat.HP);
-                    Thread.Sleep(3000);
+                    Sleep(4000);
                     break;
 
                 case "réputation":
                     Synthesis.synthesis("Tu as " + BlindCaptain_Stat.Reputation + " point de réputation.");
-                    Thread.Sleep(3000);
+                    Sleep(3000);
                     break;
 
                 case "XP":
                 case "experience":
                     Synthesis.synthesis("Tu as " + BlindCaptain_Stat.XP + " point de d'experience.");
-                    Thread.Sleep(3000);
+                    Sleep(3000);
                     break;
 
                 case "épée":
                 case "sabre":
                     Synthesis.synthesis("Tu as " + BlindCaptain_Stat.SwordDamage + " point de dommmage à l'épée.");
-                    Thread.Sleep(3000);
+                    Sleep(3000);
                     break;
 
                 case "pistolet":
                     Synthesis.synthesis("Tu as " + BlindCaptain_Stat.GunDamage + " point de dommage au pistolet.");
-                    Thread.Sleep(3000);
+                    Sleep(3000);
                     break;
 
                 case "sauver":
                 case "sauvegarder":
                 case "enregistrer":
-                    //script save
+                    Debug.Log("sauver");
                     break;
 
                 case "quitter":
-                    Synthesis.synthesis("Tu es sur que tu veux quitter sans sauvegarder ? Si oui appuyer sur Espace");
-                    Thread.Sleep(4000);
-                    if (Input.GetKey(KeyCode.Space))
-                    {
-                        //Scripte de save
-                    }
-
+                    Application.Quit(1);
                     break;
 
-                case "gueule":
-                case "ferme":
-                case "enculé":
-                case "connasse":
-                case "pute":
-                case "salope":
-                case "grosse":
-                    Synthesis.synthesis("Nique ta mère");
-                    Thread.Sleep(2000);
-                    break;
-
+               case "non":
+               case "rien":
+               case "merci":
+                   Synthesis.synthesis("Ok Capitaine");
+                   quit = true;
+                   break;
             }
-            
-            
+
+        if (!quit)
+        {
+            Synthesis.synthesis("Voulez vous autre chose capitaine ?"); 
         }
 
     }
@@ -111,29 +104,53 @@ public class Coco : MonoBehaviour
         }
         
         Recognition.Function OkCoco = TraitementCoco;
-        Recognition.start_recognition(OkCoco);
+        Recognition.start_recognition(OkCoco,"Coco",0);
+
+        quit = true;
         
+    }
+
+    private void Update()
+    {
+        if (activated)
+        {
+            
+            Debug.Log("coco activé");
+            Recognition.stop_recognition();
+            
+            //UnityMainThreadDispatcher.Instance().Enqueue((() => Paused()));
+            
+            Recognition.Function AnswerCoco = Answertraitement;
+            Debug.Log("nouvelle reco");
+            Recognition.start_recognition(AnswerCoco, "non , rien , merci , niveau , bateau , navire , HP , vie , XP , réputation , épée , sabre, pistolet" +
+                                                      "sauver , sauvegarder , quitter", 0);
+            activated = false;
+            Debug.Log("désactivé");
+        }
+
+        if (quit)
+        {
+            Recognition.stop_recognition();
+            quit = false;
+            Recognition.Function OkCoco = TraitementCoco;
+            Recognition.start_recognition(OkCoco,"Coco",0); 
+        }
+
     }
 
     void TraitementCoco(string mot)
     {
-        Debug.Log("Coco" + mot);
-        if (mot == "Ok Coco")
+        if (mot == "Coco")
         {
             Synthesis.synthesis("Coco Activé");
-            Time.timeScale = 0.0f;
-            
-            Recognition.stop_recognition();
-            Recognition.Function AnswerCoco = TraitementCoco;
-            Recognition.start_recognition(AnswerCoco);
+            activated = true;
         }
-        else
-        {
-            Recognition.stop_recognition();
-            Recognition.Function OkCoco = TraitementCoco;
-            Recognition.start_recognition(OkCoco); 
-        }
+       
     }
 
+    public void Paused()
+    {
+        Time.timeScale = 0.0f;
+    }
    
 }
