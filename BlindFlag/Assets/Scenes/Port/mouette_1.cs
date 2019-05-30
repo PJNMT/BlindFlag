bd9c79fd05a12cb9816dca8da6acb8ac122e5316
marius.hurbin <marius.hurbin@epita.fr>
2019-05-30 23:46:30 +0200

    ART du port

---
+++ BlindFlag/Assets/Scenes/Port/mouette_1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouette_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(Vector3.up, -20 * Time.deltaTime);
+        transform.Translate(Vector3.left * 15 * Time.deltaTime); //on avance en f° du temps
+    }
+}
--- BlindFlag/Assets/Scenes/Port/move.cs
+++ BlindFlag/Assets/Scenes/Port/move.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,70 +9,78 @@ public class move : MonoBehaviour
 
 
 {
-    private string scene; 
+    private string scene;
+
     Vector3 mov = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        scene = "";
-        Synthesis.synthesis("Ou voulez vous aller Capitaine ? A la taverne ? Au magasin ? ou bien voulez vous repartir ?");
-        Recognition.Function F1 = choix;
-        Recognition.start_recognition(F1,"taverne magasin partir quitter partons", 20);
-    }
+        this.GetComponentInChildren<AudioSource>().mute = true;
 
-
-    private void choix(string msg)
-    {
-        switch (msg)
-        {
-                case "taverne" :
-                    scene = "taverne";
-
-                    break;
-                case "magasin":
-                    scene = "magasin";
-                    
-                    break;
-                case "quitter":
-                case "partons":
-                case "partir" :
-                    Synthesis.synthesis("Voulez vous allez au prochain port ou chercher un trésor ?");
-                    Recognition.Function F2 = PouC;
-                    Recognition.start_recognition(F2,"trésor port", 20);
-                    
-                    break;
-                    
-                    
-                       
-                    
-        }
         
+        moveSpeed = 0f;
     }
+    /*scene = "";
+    Synthesis.synthesis("Ou voulez vous aller Capitaine ? A la taverne ? Au magasin ? ou bien voulez vous repartir ?");
+    Recognition.Function F1 = choix;
+    Recognition.start_recognition(F1,"taverne magasin partir quitter partons", 20);
+}
 
-    
 
-    private void PouC(string msg)
+private void choix(string msg)
+{
+    switch (msg)
     {
-        switch (msg)
-        {
-                case "port":
-                    scene = "port";
-                    
-                    break;
+            case "taverne" :
+                scene = "taverne";
+
+                break;
+            case "magasin":
+                scene = "magasin";
                 
-                case "trésor":
-                    scene = "tresor";
-                    
-                    break;
-                    
-                    
-        }
+                break;
+            case "quitter":
+            case "partons":
+            case "partir" :
+                Synthesis.synthesis("Voulez vous allez au prochain port ou chercher un trésor ?");
+                Recognition.Function F2 = PouC;
+                Recognition.start_recognition(F2,"trésor port", 20);
+                
+                break;
+                
+                
+                   
+                
     }
     
-    
-    public float moveSpeed = 20f;
+}
+
+
+
+private void PouC(string msg)
+{
+    switch (msg)
+    {
+            case "port":
+                scene = "port";
+                
+                break;
+            
+            case "trésor":
+                scene = "tresor";
+                
+                break;
+                
+                
+    }
+}
+*/
+
+    public float moveSpeed;
+
     public float turnSpeed = 50f;
+
     // Update is called once per frame
     void Update()
     {
@@ -79,67 +88,80 @@ public class move : MonoBehaviour
         {
             switch (scene)
             {
-                    case "taverne" : 
-                        
-                        SceneManager.LoadScene("taverne");
-                        BlindShip_Stat.SceneLoad = 3;
-                        SceneManager.UnloadSceneAsync("port");
-                        break;
-                    case "magasin" :
-                        
-                        SceneManager.LoadScene("ShipShop");
-                        BlindShip_Stat.SceneLoad = 6;
-                        SceneManager.UnloadSceneAsync("port");
-                        break;
-                    case "port" :
-                        SceneManager.LoadScene("navi");
-                        foreach (GameObject o in SceneManager.GetSceneByName("navi").GetRootGameObjects())
+                /*
+                                    case "taverne" : 
+                                        
+                                        SceneManager.LoadScene("taverne");
+                                        BlindShip_Stat.SceneLoad = 3;
+                                        SceneManager.UnloadSceneAsync("port");
+                                        break;
+                                    case "magasin" :
+                                        
+                                        SceneManager.LoadScene("ShipShop");
+                                        BlindShip_Stat.SceneLoad = 6;
+                                        SceneManager.UnloadSceneAsync("port");
+                                        break;*/
+                case "port":
+                    SceneManager.LoadScene("navi");
+                    foreach (GameObject o in SceneManager.GetSceneByName("navi").GetRootGameObjects())
+                    {
+                        if (o.name == "island")
                         {
-                            if (o.name == "island")
-                            {
-                                o.tag = "Le port";
-                                break;
-                            }
-                        
+                            o.tag = "Le port";
+                            break;
                         }
-                        BlindShip_Stat.SceneLoad = 0;
-                       
-                        SceneManager.UnloadSceneAsync("Port");
-                        break;
-                    case "tresor" :
-                        SceneManager.LoadScene("navi");
-                        foreach (GameObject o in SceneManager.GetSceneByName("navi").GetRootGameObjects())
-                        {
-                            if (o.name == "island")
-                            {
-                                o.tag = "L'ile au trésor";
-                                break;
-                            }
-                        
-                        }
-                        BlindShip_Stat.SceneLoad = 0;
 
-                        SceneManager.UnloadSceneAsync("Port");
-                        break;
+                    }
+
+                    BlindShip_Stat.SceneLoad = 0;
+                    SceneManager.UnloadSceneAsync("Port");
+                    break;
+                case "tresor":
+                    SceneManager.LoadScene("navi");
+                    foreach (GameObject o in SceneManager.GetSceneByName("navi").GetRootGameObjects())
+                    {
+                        if (o.name == "island")
+                        {
+                            o.tag = "L'ile au trésor";
+                            break;
+                        }
+
+                    }
+
+                    BlindShip_Stat.SceneLoad = 0;
+
+                    SceneManager.UnloadSceneAsync("Port");
+                    break;
             }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        if (Input.GetKey(KeyCode.UpArrow)) moveSpeed += 1f; //on augmente la vitesse ou on la baisse
-        if (Input.GetKey(KeyCode.DownArrow)) moveSpeed -= 1f;
-        if(Input.GetKey(KeyCode.LeftArrow)) transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-        if(Input.GetKey(KeyCode.RightArrow)) transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime); //on tourne en f° du temps et non de l'update
-        if (Input.GetKey(KeyCode.Space)) moveSpeed += 1f;
+
+
+
+
+        moveSpeed = 0f;
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+
+
+
+        // if (Input.GetKey(KeyCode.UpArrow)) moveSpeed += 1f; //on augmente la vitesse ou on la baisse
+        // if (Input.GetKey(KeyCode.DownArrow)) moveSpeed -= 1f;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("bite");
+            this.GetComponentInChildren<AudioSource>().mute = false;
+
+            
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) this.GetComponentInChildren<AudioSource>().mute = true;
+            
+            //transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime); //on tourne en f° du temps et non de l'update
+            // if (Input.GetKey(KeyCode.Space)) moveSpeed += 1f;
 
         if (moveSpeed < 0f) moveSpeed = 0f;
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime); //on avance en f° du temps
-        
+
 /*      transform.position += mov;
         if (transform.position.x>45)
         {
@@ -151,19 +173,19 @@ public class move : MonoBehaviour
             mov.x = 0.1f;
         }
 */
-        
-        
-        
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name == "Ship-Shop")
-        {
-            
-            Debug.Log("PIPIPIPI");
 
-            //SceneManager.LoadScene("chasseautrésor");
+
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.name == "Ship-Shop")
+            {
+
+                Debug.Log("PIPIPIPI");
+
+                //SceneManager.LoadScene("chasseautrésor");
+            }
         }
     }
-}
