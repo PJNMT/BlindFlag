5f26ce743a128df0f74e82151163649f90794ad9
marius.hurbin <marius.hurbin@epita.fr>
2019-05-30 14:25:56 +0200

    ART du port

---
+++ BlindFlag/Assets/Scenes/Port/mouette.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouette : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(Vector3.up, 100 * Time.deltaTime);
+        transform.Translate(Vector3.left * 5 * Time.deltaTime); //on avance en f° du temps
+    }
+}
