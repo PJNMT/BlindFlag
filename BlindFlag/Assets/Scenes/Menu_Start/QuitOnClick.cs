4b99b653818338adf3348e3c8e3c0e6cbccc88b4
Tao Blancheton <tao.blancheton@epita.fr>
2019-06-03 15:50:13 +0200

    corr

--- BlindFlag/Assets/Scenes/Menu_Start/QuitOnClick.cs
+++ BlindFlag/Assets/Scenes/Menu_Start/QuitOnClick.cs
@@ -7,10 +7,10 @@ public class QuitOnClick : MonoBehaviour
 
     public void Quit()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        Application.Quit ();
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit ();
+        #endif
     }
 }
