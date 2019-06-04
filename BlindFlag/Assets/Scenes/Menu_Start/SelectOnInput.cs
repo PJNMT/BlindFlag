2cd9a78cd298e7208057a3a6ce2e318374ff646a
Lyrianna <lambrinephelie@hotmail.fr>
2019-06-04 23:29:11 +0200

    voix menu + scripts vocaux

--- BlindFlag/Assets/Scenes/Menu_Start/SelectOnInput.cs
+++ BlindFlag/Assets/Scenes/Menu_Start/SelectOnInput.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Threading;
 using UnityEngine.EventSystems;
 
 public class SelectOnInput : MonoBehaviour {
 
     public EventSystem eventSystem;
     public GameObject selectedObject;
+    
+    public AudioClip Continue;
+    public AudioClip NewGame;
+    public AudioClip Options;
+    public AudioClip Quit;
+    public AudioClip Back;
+
+    public AudioSource Audio;
 
     private bool buttonSelected;
 
     // Use this for initialization
-    void Start () {
-    
+    void Start ()
+    {
+        Audio = GetComponent<AudioSource>();
     }
     
     // Update is called once per frame
@@ -21,6 +31,31 @@ public class SelectOnInput : MonoBehaviour {
         {
             eventSystem.SetSelectedGameObject(selectedObject);
             buttonSelected = true;
+
+            /*string name = selectedObject.name;
+
+            if (name == "ContinuButton")
+            {
+                Audio.PlayOneShot(Continue);
+            }
+            else if (name == "StartButton")
+            {
+                Audio.PlayOneShot(NewGame);
+            }
+            else if (name == "Options")
+            {
+                Audio.PlayOneShot(Options);
+            }
+            else if (name == "QuitButton")
+            {
+                
+                Audio.PlayOneShot(Quit);
+            }
+            else if (name == "BackButton")
+            {
+                Audio.PlayOneShot(Back);
+            }*/
+            
         }
     }
 
